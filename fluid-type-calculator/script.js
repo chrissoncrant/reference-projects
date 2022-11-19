ccc021254776145f81f58027262d8133a21ada62
Chris Soncrant <christopher.soncrant@gmail.com>
2022-11-18 16:01:18 -0800

    updated favicons; added html for fluid type calculator

--- fluid-type-calculator/script.js
+++ fluid-type-calculator/script.js
@@ -1,4 +1,28 @@
 //Inputs are in pixels
+const minFontSize = document.querySelector('.minFontSize');
+
+const maxFontSize = document.querySelector('.maxFontSize');
+
+const minFontEnd = document.querySelector('.minFontEnd');
+
+const maxFontStart = document.querySelector('.maxFontStart');
+
+const units = document.querySelectorAll('[type="radio"]');
+
+function getSelectedUnit(units) {
+    return [...units].filter(unit => unit.checked)[0].value;
+}
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const unit = getSelectedUnit(units);
+    
+    const result = calculateFluidType(minFontSize.value, maxFontSize.value, minFontEnd.value, maxFontStart.value, unit)
+
+    const p = document.querySelector('.answer');
+    p.textContent = result;
+})
 
 function calculateFluidType(minFontSize, maxFontSize, minFontEnd, maxFontStart, unit='px') {
     const rateChange = ((100 * (maxFontSize - minFontSize)) / (maxFontStart - minFontEnd)).toFixed(3);
@@ -12,4 +36,7 @@ function calculateFluidType(minFontSize, maxFontSize, minFontEnd, maxFontStart, 
     }
 }
 
-console.log(calculateFluidType(16, 18, 721, 900, 'rem'))
+console.log(calculateFluidType(28, 58, 720, 1050, 'rem'))
+
+//clamp(28px, 9.091vw + -37.455px, 58px)
+// clamp(1.750rem, 9.091vw + -2.341rem, 3.625rem)
