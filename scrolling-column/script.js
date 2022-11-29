c7e72ebf6d49fe2f1b755d4f8e4fc879051aa1d8
Christopher Soncrant <christopher.soncrant@gmail.com>
2022-11-28 16:07:26 -0800

    added scrolling-columns project

---
+++ scrolling-column/script.js
@@ -0,0 +1,13 @@
+const ATM_STATE = {
+    totalAmount: 628,
+    hundred: 300,
+    fifty: 150,
+    twenty: 80,
+    ten: 40,
+    five: 25,
+    one: 33,
+  };
+
+  const copy = {...ATM_STATE};
+
+  console.log(copy)
